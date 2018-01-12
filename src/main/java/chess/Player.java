8762b1cba1b3e5fea7d242c469d1c1c534915b51
martin morissette <martin_morissette@outlook.com>
2018-01-11 21:13:58 -0500

    Refactored and added javadoc

--- src/main/java/chess/Player.java
+++ src/main/java/chess/Player.java
@@ -1,73 +1,10 @@
 package chess;
 
-import java.util.NavigableMap;
-import java.util.TreeMap;
 
 /**
  * This class is responsible for representing a chess player.
  */
 public class Player {
-
-   /**
-    * This map is used to fetch the probability value based on the rating difference between two chess players.
-    */
-   private final static NavigableMap<Integer, Float> map;
-
-   static {
-      map = new TreeMap<Integer, Float>();
-      map.put(0, 0.50F);
-      map.put(4, 0.51F);
-      map.put(11, 0.52F);
-      map.put(18, 0.53F);
-      map.put(26, 0.54F);
-      map.put(33, 0.55F);
-      map.put(40, 0.56F);
-      map.put(47, 0.57F);
-      map.put(54, 0.58F);
-      map.put(62, 0.59F);
-      map.put(69, 0.60F);
-      map.put(77, 0.61F);
-      map.put(84, 0.62F);
-      map.put(92, 0.63F);
-      map.put(99, 0.64F);
-      map.put(107, 0.65F);
-      map.put(114, 0.66F);
-      map.put(122, 0.67F);
-      map.put(130, 0.68F);
-      map.put(138, 0.69F);
-      map.put(146, 0.70F);
-      map.put(154, 0.71F);
-      map.put(163, 0.72F);
-      map.put(171, 0.73F);
-      map.put(180, 0.74F);
-      map.put(189, 0.75F);
-      map.put(198, 0.76F);
-      map.put(207, 0.77F);
-      map.put(216, 0.78F);
-      map.put(226, 0.79F);
-      map.put(236, 0.80F);
-      map.put(246, 0.81F);
-      map.put(257, 0.82F);
-      map.put(268, 0.83F);
-      map.put(279, 0.84F);
-      map.put(291, 0.85F);
-      map.put(303, 0.86F);
-      map.put(316, 0.87F);
-      map.put(329, 0.88F);
-      map.put(345, 0.89F);
-      map.put(358, 0.90F);
-      map.put(375, 0.91F);
-      map.put(392, 0.92F);
-      map.put(412, 0.93F);
-      map.put(433, 0.94F);
-      map.put(457, 0.95F);
-      map.put(485, 0.96F);
-      map.put(518, 0.97F);
-      map.put(560, 0.98F);
-      map.put(620, 0.99F);
-      map.put(735, 1.00F);
-   }
-
 
    public String prenom;
 
@@ -87,6 +24,7 @@ public class Player {
 
    public int ties = 0;
 
+
    public Player(String prenom, String nom, float rating) {
       this.nom = nom;
       this.prenom = prenom;
@@ -94,31 +32,5 @@ public class Player {
       this.rating = rating;
       this.oldRating = rating;
    }
-
-   /**
-    * This method is used to calculate a rating adjustment (delta) based on the result from a game
-    * between two chess players.
-    *
-    * @param opponent     Player instance.
-    * @param actualResult Result : 1F win, 0F loss, 0.5F tie
-    * @return The rating variation.
-    */
-   public float getDeltaFromGame(Player opponent, float actualResult) {
-
-      Integer diff = Math.round(this.rating - opponent.rating);
-      float expectedResult = 0;
-
-      // To do a lookup for some value in 'key'
-      if (diff < 0) {
-         expectedResult = 1 - map.floorEntry(Math.abs(diff)).getValue();
-      } else {
-         expectedResult = map.floorEntry(diff).getValue();
-      }
-
-      float delta = 32 * (actualResult - expectedResult);
-
-      return delta;
-   }
-
 
 }
--- src/main/java/chess/Tournament.java
+++ src/main/java/chess/Tournament.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 
@@ -14,7 +13,6 @@ import java.util.List;
  * This class is responsible for representing a chess tournament composed of a list of Player and Game.
  */
 public class Tournament {
-
 
    private List<Player> players = new ArrayList<>();
 
@@ -40,18 +38,21 @@ public class Tournament {
       return Arrays.copyOf(playersStanding, playersStanding.length);
    }
 
+
    public void addRound(Round round) {
       rounds.add(round);
-      for (Game game : round.getGames()) {
-         addResult(game);
-      }
    }
 
-
+   /**
+    * Method used to add a game result to each player metrics and the computed rating adjustments for the
+    * respective players in the resultMatrix of the tournament.
+    * @param game The added game.
+    */
    public void addResult(Game game) {
       int coordPlayer1 = players.indexOf(game.player1);
       int coordPlayer2 = players.indexOf(game.player2);
       float result2 = game.result;
+
       if (game.result == 1F) {
          result2 = 0;
          game.player1.wins += 1;
@@ -63,29 +64,55 @@ public class Tournament {
          game.player2.wins += 1;
          game.player2.score += 1F;
          result2 = 1;
+
       } else {
          game.player1.ties += 1;
          game.player2.ties += 1;
          game.player1.score += 0.5F;
          game.player2.score += 0.5F;
       }
-      resultMatrix[coordPlayer1][coordPlayer2] += game.player1.getDeltaFromGame(game.player2, game.result);
-      resultMatrix[coordPlayer2][coordPlayer1] += game.player2.getDeltaFromGame(game.player1, result2);
+
+      resultMatrix[coordPlayer1][coordPlayer2] += Game.getDeltaFromGame(game.player1, game.player2, game.result);
+      resultMatrix[coordPlayer2][coordPlayer1] += Game.getDeltaFromGame(game.player2, game.player1, result2);
    }
 
-
+   /**
+    * Method used to aggregate all the rating adjustments for each player and compute the final rating after
+    * a completed tournament. The bonus is also calculated based on the number of rounds played.
+    */
    public void computeNewRatings() {
+
+      for (Round round : rounds) {
+         for (Game game : round.getGames()) {
+            addResult(game);
+         }
+      }
+
       for (Player player : players) {
          float newRating = player.rating;
          for (int i = 0; i < players.size(); i++) {
             newRating += resultMatrix[players.indexOf(player)][i];
          }
+
+         // Calculate bonus if 4 rounds or more were played
+
          player.oldRating = player.rating;
-         player.rating = newRating;
+         float delta = newRating - player.oldRating;
+         float bonus = 0;
+         if (rounds.size() > 3) {
+            bonus = delta - (float)(24 + 2 * (rounds.size() - 4));
+            if (bonus < 0F) {
+               bonus = 0F;
+            }
+         }
+         player.rating = newRating + bonus;
       }
       computePlayerStanding();
    }
 
+   /**
+    * Method used to compute the standing of the player and sort the list.
+    */
    public void computePlayerStanding() {
       for (int i = 0; i < players.size(); i++) {
          playersStanding[i] = players.get(i);
@@ -93,8 +120,7 @@ public class Tournament {
       insertionsortOnScore(playersStanding);
    }
 
-
-   public void printTournamentReport() {
+    public void printTournamentReport() {
       System.out.println("*********************");
       System.out.println("* Tournament report *");
       System.out.println("*********************");
@@ -108,6 +134,11 @@ public class Tournament {
       }
    }
 
+   /**
+    *
+    * @param fileName
+    * @throws IOException
+    */
    public void printTournamentReportToCsvFile(String fileName) throws IOException {
 
       try (BufferedWriter bufferedWriter = new BufferedWriter(
@@ -126,7 +157,10 @@ public class Tournament {
       }
    }
 
-
+   /**
+    *
+    * @param players
+    */
    public static void insertionsortOnScore(Player[] players) {
       int n = players.length;
       for (int k = 1; k < n; k++) {
